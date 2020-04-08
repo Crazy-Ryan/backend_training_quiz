71e75aff3c4862c9f19edf6656375534787035f2
Ryan.Wang <ranwang1995@outlook.com>
2020-04-08 18:40:13 +0800

    [WangRan] feat: add class ParkingLot

---
+++ src/main/java/entity/ParkingLot.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class ParkingLot {
+    private char id;
+    private int capacity;
+
+    public char getId() {
+        return id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
