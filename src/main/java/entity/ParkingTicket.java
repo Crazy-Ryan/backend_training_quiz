9a8a39912b6a0526d72c1232e1f6aaabd57cd8ce
Ryan.Wang <ranwang1995@outlook.com>
2020-04-08 18:42:42 +0800

    [WangRan] feat: add class ParkingTicket

---
+++ src/main/java/entity/ParkingTicket.java
@@ -0,0 +1,19 @@
+package entity;
+
+public class ParkingTicket {
+    private char parkingLotId;
+    private int spaceNo;
+    private String licensePlateNo;
+
+    public char getParkingLotId() {
+        return parkingLotId;
+    }
+
+    public int getSpaceNo() {
+        return spaceNo;
+    }
+
+    public String getLicensePlateNo() {
+        return licensePlateNo;
+    }
+}
