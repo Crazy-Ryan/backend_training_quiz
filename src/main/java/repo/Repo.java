affdbe1a0b39123cb5a78ab42c97b90ecbca0486
Ryan.Wang <ranwang1995@outlook.com>
2020-04-08 21:25:10 +0800

    [WangRan] feat: add JDBC manipulation methods

---
+++ src/main/java/repo/Repo.java
@@ -0,0 +1,15 @@
+package repo;
+
+import entity.ParkingLot;
+
+import java.util.List;
+
+public class Repo {
+    public void init(List<ParkingLot> parkingLots) {
+
+    }
+
+    private void emptyDb(){
+
+    }
+}
